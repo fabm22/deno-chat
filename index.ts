bd9a41c16b431040f106b4e8701d5d5eb8485ddf
Fab <fmrage@hotmail.com>
2021-07-09 01:27:12 +0200

    update2

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Application } from 'https://deno.land/x/oak/mod.ts';
 const app = new Application();
 
 app.use((ctx) => {
-    ctx.response.body = 'Hello wooorld';
+    ctx.response.body = 'Hello wooorld with Oak.';
 })
 
 
