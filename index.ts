1cf39e2ff3e0c530a24df655f43091dae1f8a0b8
Fab <fmrage@hotmail.com>
2021-07-15 18:06:14 +0200

    update 3

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+// import {  } from "https://deno.land/std@0.99.0/http/http_status.ts";
 
 const app = new Application();
 
@@ -6,8 +7,31 @@ app.use((ctx) => {
     ctx.response.body = 'Hello wooorld with Oak.';
 })
 
+const messages : string [] = []; // new Map<string, any>();
 
-addEventListener('fetch', app.fetchEventHandler())
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  .get("/messages",  (context) => {
+    context.response.body = messages; // Array.from(messages.values());
+  })
+  .post("/messages", async (context) => {
+      const msg = await context.request.body().value;
+      context.response.body = msg;
+  })
+;
+
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+
+
+// addEventListener('fetch', app.fetchEventHandler())
 
 // // window.
 // addEventListener('fetch', (event) => {
